f210e4b44db8eb9a028c59a9149d3f7fc3f90db0
Tharindu <tharindui@labfriend.com>
2022-12-01 21:43:28 +0530

    rrr

--- express/server.js
+++ express/server.js
@@ -1,13 +1,12 @@
-"use strict";
 const express = require("express");
 const mongoose = require("mongoose");
-const path = require("path");
+//const path = require("path");
 const serverless = require("serverless-http");
 const app = express();
-const cors = require("cors");
+//const cors = require("cors");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
-app.use(cors());
+//app.use(cors());
 const router = express.Router();
 require("dotenv/config");
 const Employee = require("../models/employee");
@@ -89,9 +88,9 @@ router.post("/", async (req, res) => {
 });
 
 app.use("/.netlify/functions/server", router); // path must route to lambda
-app.use("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
+//app.use("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => {
   console.log("db connected ........");
 });
-module.exports = app;
+//module.exports = app;
 module.exports.handler = serverless(app);
