edce08f78b25ef4aa52219b956fef31d20cf2050
Tharindu <tharindui@labfriend.com>
2022-12-01 21:35:47 +0530

    all to server

--- express/server.js
+++ express/server.js
@@ -10,11 +10,84 @@ app.use(bodyParser.json());
 app.use(cors());
 const router = express.Router();
 require("dotenv/config");
-const employeeRoutes = require("./routes/employee");
+const Employee = require("../models/employee");
 
-//middleware
-app.use("/employee", employeeRoutes);
 app.use(bodyParser.json());
+
+//GET ALL EMPLOYEES
+router.get("/", async (req, res) => {
+  try {
+    const employees = await Employee.find();
+    res.json(employees);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+//GET EMPLOYEE BY ID
+router.get("/:empId", async (req, res) => {
+  try {
+    const employee = await Employee.findById(req.params.empId);
+    res.json(employee);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+//REMOVE EMPLOYEE
+router.delete("/:empId", async (req, res) => {
+  try {
+    const removedEmployee = await Employee.remove({
+      _id: req.params.empId,
+    });
+    res.json(removedEmployee);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+//UPDATE EMPLOYEE
+router.patch("/:empId", async (req, res) => {
+  try {
+    const updatedEmployee = await Employee.updateOne(
+      {
+        _id: req.params.empId,
+      },
+      {
+        $set: {
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          email: req.body.email,
+          number: req.body.number,
+          gender: req.body.gender,
+          photo: req.body.photo,
+        },
+      }
+    );
+    res.json(updatedEmployee);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+//ADD NEW EMPLOYEE
+router.post("/", async (req, res) => {
+  const employee = new Employee({
+    firstName: req.body.firstName || "",
+    lastName: req.body.lastName || "",
+    email: req.body.email || "",
+    number: req.body.number || "",
+    gender: req.body.gender || "",
+    photo: req.body.photo || "",
+  });
+  try {
+    const savedEmployee = await employee.save();
+    res.json(savedEmployee);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
 app.use("/.netlify/functions/server", router); // path must route to lambda
 app.use("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => {
