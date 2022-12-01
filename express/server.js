982047b3aed97ebf2fbb3f6940be3a9c841d1c07
Tharindu <tharindui@labfriend.com>
2022-12-01 22:29:22 +0530

    error

--- express/server.js
+++ express/server.js
@@ -1,22 +1,17 @@
-"use strict";
 const express = require("express");
 const mongoose = require("mongoose");
-//const path = require("path");
 const serverless = require("serverless-http");
 const app = express();
 const cors = require("cors");
+require("dotenv/config");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(cors());
-//const router = express.Router();
-require("dotenv/config");
+
 app.use(bodyParser.json());
 mongoose.connect(
   "mongodb+srv://tharinduI:1122Tharidu@employeecluster.mt38mgw.mongodb.net/employee?retryWrites=true&w=majority",
-  { useNewUrlParser: true },
-  () => {
-    console.log("db connected ........");
-  }
+  { useNewUrlParser: true }
 );
 
 const employeeRoutes = require("./routes/employee");
@@ -24,8 +19,9 @@ const employeeRoutes = require("./routes/employee");
 //middleware
 app.use("/.netlify/functions/server", employeeRoutes);
 
-module.exports = app;
-module.exports.handler = serverless(app);
+//module.exports = app;
+const handler = serverless(app);
+//module.exports.handler = serverless(app);
 module.exports.handler = async (event, context) => {
   const result = await handler(event, context);
   return result;
