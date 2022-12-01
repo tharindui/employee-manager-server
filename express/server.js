d162bf40bc9684da373f97cb7ed1cf67832665be
Tharindu <tharindui@labfriend.com>
2022-12-01 22:26:00 +0530

    bbbb

--- express/server.js
+++ express/server.js
@@ -1,19 +1,23 @@
 "use strict";
 const express = require("express");
 const mongoose = require("mongoose");
-const path = require("path");
+//const path = require("path");
 const serverless = require("serverless-http");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(cors());
-const router = express.Router();
+//const router = express.Router();
 require("dotenv/config");
 app.use(bodyParser.json());
-mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => {
-  console.log("db connected ........");
-});
+mongoose.connect(
+  "mongodb+srv://tharinduI:1122Tharidu@employeecluster.mt38mgw.mongodb.net/employee?retryWrites=true&w=majority",
+  { useNewUrlParser: true },
+  () => {
+    console.log("db connected ........");
+  }
+);
 
 const employeeRoutes = require("./routes/employee");
 
