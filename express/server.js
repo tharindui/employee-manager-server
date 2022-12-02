4c0ffea7815337f9edf0f6ca5b7eb80b4cb6a1a6
Tharindu <tharindui@labfriend.com>
2022-12-02 11:35:45 +0530

    tttt

--- express/server.js
+++ express/server.js
@@ -9,14 +9,27 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use(bodyParser.json());
-mongoose.connect(
-  "mongodb+srv://tharinduI:1122Tharidu@employeecluster.mt38mgw.mongodb.net/employee?retryWrites=true&w=majority",
-  { useNewUrlParser: true }
-);
+
+app.get("/", async (request, response) => {
+  console.log("hey");
+  response.send({ hello: "world" });
+});
 
 const employeeRoutes = require("./routes/employee");
 
 //middleware
-app.use("/employee", employeeRoutes);
+app.use("/.netlify/functions/server", employeeRoutes);
 module.exports = app;
-module.exports.handler = serverless(app);
+//module.exports.handler = serverless(app);
+const handler = serverless(app);
+module.exports.handler = async (event, context) => {
+  // mongoose.connect(process.env.DATABASE_URL);
+  mongoose.connect(
+    "mongodb+srv://tharinduI:1122Tharidu@employeecluster.mt38mgw.mongodb.net/employee?retryWrites=true&w=majority",
+    { useNewUrlParser: true }
+  );
+  // you can do other things here
+  const result = await handler(event, context);
+  // and here
+  return result;
+};
